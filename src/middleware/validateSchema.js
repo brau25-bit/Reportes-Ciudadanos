3bd52c5915c5d959f5016c2612652fa70be8fd2d
brau25-bit <brauliochaveza@gmail.com>
2026-03-01 20:41:27 -0600

    Se añadio validacion de datos y un middleware para validar estos

---
+++ src/middleware/validateSchema.js
@@ -0,0 +1,12 @@
+
+export function validateSchema(schema){
+    return (req,res,next) => {
+        const result = schema.safeParse(req.body)
+
+        if(!result.success) return res.status(400).json( {error: result.error.flatten()} )
+
+        req.validated = result.data
+
+        next()        
+    }
+}
---
+++ src/schemas/users.schema.js
@@ -0,0 +1,31 @@
+import z from 'zod'
+
+export const userSchema = z.object({
+    email: z.string()
+    .email("Formato de email invalido")
+    .min(6, "Email demasiado corto")
+    .max(255, "Email demasiado largo"),
+    
+    password: z.string()
+    .min(6, "La contraseña es muy corta")
+    .max(255, "La contraseña es demasiado larga"),
+
+    name: z.string()
+    .min(3, "Nombre demasiado corto")
+    .max(255, "Nombre demasiado largo"),
+
+    lastname: z.string()
+    .min(3, "Apellido demasiado corto")
+    .max(255, "Apellido demasiado largo")
+    .optional(),
+
+    role: z.enum(["USER", "ADMIN", "TECHNICIAN"])
+    .optional(),
+
+    banReason: z.string()
+    .min(3, "razon demasiado corta")
+    .max(500, "razon demasiado larga")
+    .optional()
+})
+
+export const partialUserSchema = userSchema.partial()
