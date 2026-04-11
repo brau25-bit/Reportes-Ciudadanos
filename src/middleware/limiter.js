2f5c211f16c95a61cf83521ce3209dec6c60ea99
brau25-bit <brauliochaveza@gmail.com>
2026-04-11 16:02:34 -0600

    add: middleware para limitar las peticiones que se realizan a los enpoints

---
+++ src/middleware/limiter.js
@@ -0,0 +1,25 @@
+import {rateLimit} from 'express-rate-limit'
+
+export const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 20,
+    message: 'Demasiadas solicitudes, intenta más tarde',
+    standardHeaders: 'draft-8',
+    legacyHeaders: false,
+})
+
+export const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 5,  // Cambiado de 'max' a 'limit'
+    message: 'Demasiados intentos de login, intenta más tarde',
+    standardHeaders: 'draft-8',
+    legacyHeaders: false
+})
+
+export const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 3,  // Más estricto para recuperación y verificación
+    message: 'Demasiadas solicitudes, intenta más tarde',
+    standardHeaders: 'draft-8',
+    legacyHeaders: false
+})
