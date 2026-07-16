68b8da505c62577759dc75eba2cad6077cd7b946
brau25-bit <brauliochaveza@gmail.com>
2026-07-15 20:47:45 -0600

    update: eliminacion de codigo innecesario

--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -1,9 +1,6 @@
 export function authorization(...allowedRoles){
     return (req, res, next) => {
         try {
-
-            console.log(req.user.id)
-            console.log(req.user.role)
             
             if(!req.user) {
                 return res.status(401).json({
