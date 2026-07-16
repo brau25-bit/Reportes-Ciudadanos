7c3290b2efb848934300f27d5a327af5f48f4607
brau25-bit <brauliochaveza@gmail.com>
2026-07-15 20:52:06 -0600

    update: implementacion de rutas nuevas para reportes

--- src/modules/reports/report.router.js
+++ src/modules/reports/report.router.js
@@ -2,21 +2,21 @@ import { Router } from "express";
 import { ReportController } from "./report.contoller.js";
 import { verifyToken } from "../../middleware/auth.js";
 import { validateSchema } from "../../middleware/validateSchema.js";
-import {reportSchema, partialReportSchema} from '../../schemas/users.schema.js'
+import {reportSchema, partialReportSchema} from '../../schemas/report.schema.js'
 import {authorization} from '../../middleware/authorize.js'
 import { limiter } from "../../middleware/limiter.js";
 
 const reportRouter = Router()
 
-reportRouter.get('/', verifyToken, ReportController.getReports)
-reportRouter.get('/:id', verifyToken, ReportController.getReportsByID)
-reportRouter.get('/:id/history', verifyToken, authorization('ADMIN', 'USER'), ReportController.getReportsHistory)
+reportRouter.get('/', limiter, verifyToken, ReportController.getReports)
+reportRouter.get('/:id', limiter, verifyToken, ReportController.getReportsByID)
+reportRouter.get('/:id/history', limiter, verifyToken, authorization('ADMIN', 'USER'), ReportController.getReportsHistory)
 
 reportRouter.patch('/:id', limiter, verifyToken, authorization('USER'), validateSchema(partialReportSchema), ReportController.updateReports)
 
-reportRouter.patch('/:id/delete', verifyToken, authorization('USER', 'ADMIN'), ReportController.deleteReports)
+reportRouter.patch('/:id/delete', limiter, verifyToken, authorization('USER', 'ADMIN'), ReportController.deleteReports)
 
-reportRouter.patch('/:id/status', verifyToken, authorization('ADMIN'), validateSchema(partialReportSchema), ReportController.updateReportStatus)
+reportRouter.patch('/:id/status', limiter, verifyToken, authorization('ADMIN'), validateSchema(partialReportSchema), ReportController.updateReportStatus)
 
 reportRouter.post('/', limiter, verifyToken, authorization('USER'), validateSchema(reportSchema), ReportController.createReports)
 
