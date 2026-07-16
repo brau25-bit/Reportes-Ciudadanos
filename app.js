1662d9a33db513c5d53cc6f39a42f38de8246743
brau25-bit <brauliochaveza@gmail.com>
2026-07-15 20:33:26 -0600

    update: importaciones de enrutadores, creacion de las rutas mediante express

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 import express from 'express'
+import authRouter from './src/modules/auth/auth.router.js'
+import reportRouter from './src/modules/reports/report.router.js'
+import usersRouter from './src/modules/users/users.router.js'
+import analyticsRouter from './src/modules/analytics/analytics.router.js'
+import servicesRouter from './src/modules/services/services.router.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 app.disable("x-powered-by")
+app.use(cors())
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/reports', reportRouter)
+app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/analytics', analyticsRouter)
+app.use('/api/v1/services', servicesRouter)
 
 export default app
